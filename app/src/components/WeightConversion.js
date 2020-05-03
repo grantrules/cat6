2106e9e67ec096014a9d8cb45da1183a1e689cf7
Grant Harding <gharding@gmail.com>
2020-05-03 19:29:31 -0400

    fix weight conversion css

--- app/src/components/WeightConversion.js
+++ app/src/components/WeightConversion.js
@@ -2,17 +2,17 @@ import React from 'react';
 
 function WeightConversion({ name, weightObj: { weight, isKg }, update }) {
 
-  const kgToLbs = (n) => n * 1/0.453592;
+  const kgToLbs = (n) => n * 1 / 0.453592;
   const lbsToKg = (n) => n * 0.453592;
   const radioChange = ({ weight, isKg }) => update({ weight, isKg });
 
   const onChange = ({ target: { value } }) => {
-    const weight = isKg ? value : (Math.round(lbsToKg(value)*10)/10);
+    const weight = isKg ? value : (Math.round(lbsToKg(value) * 10) / 10);
     update({ weight, isKg });
   }
 
   return (<>
-  {/*}
+    {/*}
     <ul>
       <li>weight: {weight}</li>
       <li>kg: {weight}</li>
@@ -23,14 +23,16 @@ function WeightConversion({ name, weightObj: { weight, isKg }, update }) {
 
     <div className="weightconversion">
       <input type="number" name={name} value={isKg ? weight : Math.round(kgToLbs(weight))} onChange={onChange} />
-      <label>
-        <input type="radio" checked={isKg} onChange={() => radioChange({ isKg: true, weight: Math.round(kgToLbs(weight)) })} />
-        <span>kg</span>
-      </label>
-      <label>
-        <input type="radio" checked={!isKg} onChange={() => radioChange({ isKg: false, weight: Math.round(lbsToKg(weight)*10)/10 })} />
-        <span>lbs</span>
-      </label>
+      <div>
+        <label>
+          <input type="radio" checked={isKg} onChange={() => radioChange({ isKg: true, weight: Math.round(kgToLbs(weight)) })} />
+          <span>kg</span>
+        </label>
+        <label>
+          <input type="radio" checked={!isKg} onChange={() => radioChange({ isKg: false, weight: Math.round(lbsToKg(weight) * 10) / 10 })} />
+          <span>lbs</span>
+        </label>
+      </div>
     </div>
   </>)
 
